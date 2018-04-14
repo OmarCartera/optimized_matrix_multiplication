a4ef8aa6f89d39d913acd84afa076bec685c93d7
omarcartera <c.omargamal@gmail.com>
2018-04-14 16:19:09 +0200

    performance gain raised to x3.54

--- main.c
+++ main.c
@@ -16,19 +16,22 @@ int main()
     float   d[MAX_DIM][MAX_DIM] __attribute__ ((aligned(16)));
 
     __m128 row, col, result;
+    __m128 tempo = {0.0, 0.0, 0.0, 0.0};
 
 
     for(i = 0; i < MAX_DIM; i++)
     {
         for(j =0; j < MAX_DIM; j++)
         {
-            a[i][j] = 1.7;
+            a[i][j] = 1;
 
             c[i][j] = 0; // Necessary Initialization - Don't change
             d[i][j] = 0; // Necessary Initialization - Don't change
         }
     }
 
+
+    clock_t time5 = clock();
 
     // transposing
     for(i = 0; i < MAX_DIM; i++)
@@ -38,6 +41,8 @@ int main()
             b[j][i] = a[i][j];
         }
     }
+
+    clock_t time6 = clock();
 
 
     // unoptimized matrix multiplication
@@ -75,24 +80,42 @@ int main()
                 col = _mm_load_ps(&(b[j][k]));
 
                 result  = _mm_mul_ps(row, col);
+
+                tempo = _mm_add_ps(tempo, result);
+
+                //printf("%.2f %.2f %.2f %.2f\n", tempo[0], tempo[1], tempo[2], tempo[3]);
                 
-                temp += (result[0] + result[1] + result[2] + result[3]);
             }
             
-            d[i][j] = temp;
+            d[i][j] = (tempo[0] + tempo[1] + tempo[2] + tempo[3]);
+            tempo[0] = 0.0; tempo[1] = 0.0; tempo[2] = 0.0; tempo[3] = 0.0;
+
         }
     }
 
     clock_t time4 = clock();
 
+    /*
+    for(i = 0; i < MAX_DIM; i++)
+    {
+        for(j = 0; j < MAX_DIM; j++)
+        {
+            if(c[i][j] != d[i][j])
+                printf("ya5taaaaaaaaaaaaaaaaaaaaay\n");
+        }
+    }
+    */
+
 
     // calculate and print execution times
     double TotalTimeLoop = ((double) time2 - (double) time1) / CLOCKS_PER_SEC;
     double TotalTimeSIMD = ((double) time4 - (double) time3) / CLOCKS_PER_SEC;
+    double TransposeTime = ((double) time6 - (double) time5) / CLOCKS_PER_SEC;
 
-    printf(" Time taken by loop is %.7f seconds\n", TotalTimeLoop);
-    printf(" Time taken by SIMD optimized code is %.7f seconds\n", TotalTimeSIMD);
-    printf("Performance gain is x%0.2f\n", TotalTimeLoop/TotalTimeSIMD);
+    printf("Time taken by loop is %.7f seconds\n", TotalTimeLoop);
+    printf("Time taken by SIMD optimized code is %.7f seconds\n", TotalTimeSIMD);
+    printf("Performance gain is x%0.2f\n\n", TotalTimeLoop/TotalTimeSIMD);
+    printf("Transpose time is %0.7f seconds\n\n", TransposeTime);
 
     return 0;
 }
